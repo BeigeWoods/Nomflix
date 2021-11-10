3022655c96f93fb6d26f8e0dfbb8de518a77a3ad
BeigeWoods <juhyunkbstar@gmail.com>
2021-11-10 20:59:04 +0900

    modified DetailContainer

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -4,6 +4,10 @@ import { useState, useEffect } from "react";
 const DetialContainer = (props) => {
   const {
     location: { pathname },
+    match: {
+      params: { id },
+    },
+    history: { push },
   } = props;
   const [state, setState] = useState({
     result: null,
@@ -11,33 +15,27 @@ const DetialContainer = (props) => {
     loading: true,
     isMovie: pathname.includes("/movie/"),
   });
-  const getData = async () => {
-    const {
-      match: {
-        params: { id },
-      },
-      history: { push },
-    } = props;
-    const parsedId = parseInt(id);
-    if (isNaN(parsedId)) {
-      return push("/");
-    }
-    let { result, isMovie } = state;
-    try {
-      if (isMovie) {
-        ({ data: result } = await moviesApi.movieDetail(parsedId));
-      } else {
-        ({ data: result } = await tvApi.showDetail(parsedId));
-      }
-    } catch {
-      setState({ ...state, error: "Can't find anything." });
-    } finally {
-      setState({ ...state, loading: false, result });
-    }
-  };
   useEffect(() => {
+    async function getData() {
+      const parsedId = parseInt(id);
+      if (isNaN(parsedId)) {
+        return push("/");
+      }
+      let { result, isMovie } = state;
+      try {
+        if (isMovie) {
+          ({ data: result } = await moviesApi.movieDetail(parsedId));
+        } else {
+          ({ data: result } = await tvApi.showDetail(parsedId));
+        }
+      } catch {
+        setState({ ...state, error: "Can't find anything." });
+      } finally {
+        setState({ ...state, loading: false, result });
+      }
+    }
     getData();
-  }, []);
+  });
   return state;
 };
 
